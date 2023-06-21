a66d724f927459cec3c71c47bcf0f52f8c0da649
TaeHyong Kim <kthstar27@gmail.com>
2023-06-21 22:37:05 +0900

    2.0

--- EC2INS/main.tf
+++ EC2INS/main.tf
@@ -52,7 +52,7 @@ data "template_file" "EC2_USER_DATA" {
             length(var.INSs) : 0)
     template = <<-EOF
     #!/bin/bash
-    ${join("\n", [for scriptfile in var.INS_UD_FILEs[count.index] : file(scriptfile)])}
+    ${join("\n", [for FILE in var.INS_UD_FILEs[count.index] : file("${FILE}")])}
     ${var.INS_UD_SCRIPTs[count.index]}
     EOF
 }
