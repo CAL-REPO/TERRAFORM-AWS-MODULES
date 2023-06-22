a7687198f2347760a76ffb9a3d0cdf4a84542e7a
TaeHyong Kim <kthstar27@gmail.com>
2023-06-22 17:10:06 +0900

    2.0

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,13 +1,13 @@
-# Standard AWS Provider Block
-terraform {
-    required_version = ">= 1.0"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = ">= 5.0"
-        }
-    }
-}
+# # Standard AWS Provider Block
+# terraform {
+#     required_version = ">= 1.0"
+#     required_providers {
+#         aws = {
+#             source  = "hashicorp/aws"
+#             version = ">= 5.0"
+#         }
+#     }
+# }
 
 data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
