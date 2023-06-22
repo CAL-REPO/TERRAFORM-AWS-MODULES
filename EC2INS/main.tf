7d37a5e8923859124cc4a5b28e571562599b799e
TaeHyong Kim <kthstar27@gmail.com>
2023-06-22 10:51:46 +0900

    2.0

--- EC2INS/main.tf
+++ EC2INS/main.tf
@@ -36,13 +36,13 @@ resource "aws_instance" "INS" {
         }
     }
 
-    # dynamic "network_interface" {
-    #     for_each = var.INSs[count.index].AUTO_PUBLIC_IP == false ? [1] : []
-    #     content {
-    #         device_index         = 0
-    #         network_interface_id = aws_network_interface.DEFAULT_NIC[count.index].id
-    #     }
-    # }
+    dynamic "network_interface" {
+        for_each = var.INSs[count.index].AUTO_PUBLIC_IP == false ? [1] : []
+        content {
+            device_index         = 0
+            network_interface_id = aws_network_interface.DEFAULT_NIC[count.index].id
+        }
+    }
 
     user_data = data.template_file.EC2_USER_DATA[count.index].rendered
     user_data_replace_on_change = true
@@ -71,41 +71,36 @@ resource "aws_network_interface" "DEFAULT_NIC" {
     tags = {
         Name = var.INSs[count.index].AUTO_PUBLIC_IP == false ? "${var.INSs[count.index].NAME}_DEFAULT_NIC" : null
     }
+
+    # lifecycle {
+    #     create_before_destroy = true
+    # }
 }
 
-resource "aws_network_interface_attachment" "test" {
-    count = (length(var.INSs) > 0 ?
-            length(var.INSs) : 0)
+# resource "null_resource" "DELETE_UNATTACHED_NIC" {
+#     count = (length(var.INSs) > 0 ?
+#             length(var.INSs) : 0)
 
-    instance_id          = aws_instance.INS[count.index].id
-    network_interface_id = aws_network_interface.DEFAULT_NIC[count.index].id
-    device_index         = 0
-}
+#     depends_on = [ aws_instance.INS ]
+#     # Execute the deletion only if the network interface is not attached to any EC2 instance
+#     triggers = {
+#         AUTO_NIC_ID         = aws_instance.INS[count.index].primary_network_interface_id
+#         DEFAULT_NIC_ID      = aws_network_interface.DEFAULT_NIC[count.index].id
+#         always_run          = timestamp()
+#     }
 
-resource "null_resource" "DELETE_UNATTACHED_NIC" {
-    count = (length(var.INSs) > 0 ?
-            length(var.INSs) : 0)
-
-    depends_on = [ aws_instance.INS ]
-    # Execute the deletion only if the network interface is not attached to any EC2 instance
-    triggers = {
-        AUTO_NIC_ID         = aws_instance.INS[count.index].primary_network_interface_id
-        DEFAULT_NIC_ID      = aws_network_interface.DEFAULT_NIC[count.index].id
-        always_run          = timestamp()
-    }
-
-    provisioner "local-exec" {
-        command = <<-EOT
-        NIC_STATUS=$(aws ec2 describe-network-interfaces --network-interface-ids ${self.triggers.DEFAULT_NIC_ID} --query 'NetworkInterfaces[0].Status' --output text --profile=${var.PROFILE})
-        if [[ $NIC_STATUS == "available" ]]; then
-            aws ec2 delete-network-interface --network-interface-id ${self.triggers.DEFAULT_NIC_ID} --profile=${var.PROFILE}
-        fi
-        NIC_TAG=$(aws ec2 describe-network-interfaces --network-interface-ids ${self.triggers.AUTO_NIC_ID} --query 'NetworkInterfaces[0].TagSet[?Key==`Name`].Value' --output text --profile=${var.PROFILE})
-        if [[ $NIC_TAG == "" ]]; then
-            aws ec2 create-tags --resources ${self.triggers.AUTO_NIC_ID} --tags Key=Name,Value=${var.INSs[count.index].NAME}_DEFAULT_NIC --profile=${var.PROFILE}
-        fi
-        EOT
-        interpreter = ["bash", "-c"]
-        on_failure = continue
-    }
-}
+#     provisioner "local-exec" {
+#         command = <<-EOT
+#         NIC_STATUS=$(aws ec2 describe-network-interfaces --network-interface-ids ${self.triggers.DEFAULT_NIC_ID} --query 'NetworkInterfaces[0].Status' --output text --profile=${var.PROFILE})
+#         if [[ $NIC_STATUS == "available" ]]; then
+#             aws ec2 delete-network-interface --network-interface-id ${self.triggers.DEFAULT_NIC_ID} --profile=${var.PROFILE}
+#         fi
+#         NIC_TAG=$(aws ec2 describe-network-interfaces --network-interface-ids ${self.triggers.AUTO_NIC_ID} --query 'NetworkInterfaces[0].TagSet[?Key==`Name`].Value' --output text --profile=${var.PROFILE})
+#         if [[ $NIC_TAG == "" ]]; then
+#             aws ec2 create-tags --resources ${self.triggers.AUTO_NIC_ID} --tags Key=Name,Value=${var.INSs[count.index].NAME}_DEFAULT_NIC --profile=${var.PROFILE}
+#         fi
+#         EOT
+#         interpreter = ["bash", "-c"]
+#         on_failure = continue
+#     }
+# }
