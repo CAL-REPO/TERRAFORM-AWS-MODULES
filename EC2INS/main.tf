be23015f514b0368010da6e0768db217a95918b9
TaeHyong Kim <kthstar27@gmail.com>
2023-06-22 12:54:39 +0900

    2.0

--- EC2INS/main.tf
+++ EC2INS/main.tf
@@ -70,6 +70,10 @@ resource "aws_network_interface" "DEFAULT_NIC" {
     tags = {
         Name = var.INSs[count.index].AUTO_PUBLIC_IP == false ? "${var.INSs[count.index].NAME}_DEFAULT_NIC" : null
     }
+    
+    lifecycle {
+        create_before_destroy = true
+    }    
 }
 
 resource "null_resource" "MARK_NIC" {
