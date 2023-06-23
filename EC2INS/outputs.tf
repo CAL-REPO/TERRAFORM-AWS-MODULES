f1a07beb32dde3fd4db056d86bc382512e7c08da
TaeHyong Kim <kthstar27@gmail.com>
2023-06-23 11:21:50 +0900

    2.0

--- EC2INS/outputs.tf
+++ EC2INS/outputs.tf
@@ -2,11 +2,11 @@ output "ID"{
     value = aws_instance.INS[*].id
 }
 
-output "DEFAULT_NIC_ID" {
+output "PRIMARY_NIC_ID" {
     value = try(aws_instance.INS[*].primary_network_interface_id, null)
 }
 
-output "NIC_ID" {
+output "DEFAULT_NIC_ID" {
     value = try(aws_network_interface.DEFAULT_NIC[*].id, null)
 }
 
