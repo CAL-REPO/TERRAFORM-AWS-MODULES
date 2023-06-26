49f6bf2a6cc57816ae4e0d9ede8fafe67b0402b1
TaeHyong Kim <kthstar27@gmail.com>
2023-06-26 12:46:52 +0900

    2.0

--- EC2KEY/main.tf
+++ EC2KEY/main.tf
@@ -45,13 +45,13 @@ resource "aws_key_pair" "KEY" {
 
     provisioner "local-exec" {
         command = <<EOF
-            if [[ ${KEY.BACKUP_DIR} != "" ]]; then
+            if [[ ${var.KEYs[count.index].BACKUP_DIR} != "" ]]; then
                 sudo echo "${tls_private_key.PRI_KEY[count.index].private_key_pem}" > "${local.KEYs[count.index].KEY_BACKUP_FILE}"
             fi
             sudo echo "${tls_private_key.PRI_KEY[count.index].private_key_pem}" > "${local.KEYs[count.index].KEY_LINUX_FILE}"    
             sudo chmod 400 "${local.KEYs[count.index].KEY_LINUX_FILE}"
             sudo chown $USER:$USER "${local.KEYs[count.index].KEY_LINUX_FILE}"
-            if [[ ${KEY.S3_DIR} != "" ]]; then
+            if [[ ${var.KEYs[count.index].S3_DIR} != "" ]]; then
                 aws s3 cp "${local.KEYs[count.index].KEY_LINUX_FILE}" "s3://${local.KEYs[count.index].KEY_S3_FILE}"
             fi
         EOF
